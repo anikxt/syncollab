8bc1a5c18ed579243a9105addc4bc4f183aaac41
Aniket Kanere <aniket15304@gmail.com>
2024-02-06 15:29:24 +0530

    update package

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
 const cors = require('cors');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/actions/Actions');
@@ -25,6 +26,11 @@ app.use(
     },
   })
 );
+
+app.use(express.static('build'));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const userSocketMap = {};
 
